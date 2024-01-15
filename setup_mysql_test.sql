20daf8a15e559b7be4b6bfc77ace18857519d570
gregorymark254 <gregorymark254@gmail.com>
2024-01-15 22:20:11 +0300

    sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,4 @@ USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
 
--- creating new database
-CREATE DATABASE IF NOT EXISTS performance_schema;
-USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
