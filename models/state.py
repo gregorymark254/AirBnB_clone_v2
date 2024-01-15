289a1af303d71c005b7b7f14c74624d52fe84b80
gregorymark254 <gregorymark254@gmail.com>
2024-01-15 23:31:53 +0300

    state update

--- models/state.py
+++ models/state.py
@@ -2,6 +2,8 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -9,3 +11,16 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City", cascade="all, delete, delete-orphan", backref="state")
+
+
+@property
+def cities(self, state_id):
+    """Get a list of City instances with state_id equal to the given state_id"""
+    cities_list = []
+    all_cities = self.all(City)
+    for city in all_cities.values():
+        if getattr(city, 'state_id') == state_id:
+            cities_list.append(city)
+    return cities_list
