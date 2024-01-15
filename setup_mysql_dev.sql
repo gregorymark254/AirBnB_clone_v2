61e0ad2691b57f26fe30d1116d690d5725aaff8e
gregorymark254 <gregorymark254@gmail.com>
2024-01-15 22:18:00 +0300

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- creating new database
-CREATE DATABASE IF NOT EXISTS performance_schema;
-USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
