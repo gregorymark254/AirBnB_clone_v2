4339dd6a0dac2a95310d8d94c26dc5455e9ba411
gregorymark254 <gregorymark254@gmail.com>
2024-01-16 01:15:45 +0300

    Revert "file executable"
    This reverts commit a962874d4376aae4742b6404d5e3a8742e1c2912.

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-
-    name = String(Column(128), nullable=False)
+class Amenity(BaseModel):
+    name = ""
